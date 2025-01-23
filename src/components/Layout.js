c9cd625d372018b2651558f5f7f6d90bfae9031b
Bibek-arch <iloveyuhtoo063@gmail.com>
2025-01-23 12:13:29 +0545

    phonechange

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -129,10 +129,10 @@ const Layout = ({ children }) => {
                 <a href="https://www.facebook.com/surungamedicine" className="text-blue-200 hover:text-white transition duration-300 ease-in-out text-2xl">
                   <FaFacebook /> 
                 </a>
-                <a href="9804964107" className="text-blue-200 hover:text-white transition duration-300 ease-in-out text-2xl">
+                <a href="https://wa.me/9804964107" className="text-blue-200 hover:text-white transition duration-300 ease-in-out text-2xl">
                   <FaWhatsapp />
                 </a>
-                <a href="023-553097" className="text-blue-200 hover:text-white transition duration-300 ease-in-out text-2xl">
+                <a href="tel:023-553097" className="text-blue-200 hover:text-white transition duration-300 ease-in-out text-2xl">
                   <FaPhoneAlt />
                 </a>
               </div>
